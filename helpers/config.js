2f727708386da717f02c9a8bc43ac873ae04232c
sveisvei <sveinung.rosaker@gmail.com>
2016-10-11 09:33:48 +0200

    new endpoint

--- helpers/config.js
+++ helpers/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const AMQP_PORT = process.env.AMQP_PORT || 19901;
-const AMQP_URI = process.env.AMQP_URI || `amqp://0.tcp.ngrok.io:${AMQP_PORT}`;
+const AMQP_PORT = process.env.AMQP_PORT || 5672;
+const AMQP_URI = process.env.AMQP_URI || `amqp://192.168.56.94:${AMQP_PORT}`;
 
 module.exports = {
     amqpUri: AMQP_URI,
