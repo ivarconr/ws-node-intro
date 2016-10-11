339c6b1d523ba380c22a8c56d28a2fb34519d814
sveisvei <sveinung.rosaker@gmail.com>
2016-10-11 13:53:40 +0200

    add hints

--- docs/stats.js
+++ docs/stats.js
@@ -16,7 +16,7 @@ data.on('value', (snapshot) => {
     render(snapshot.val());
 });
 
-const App = ({ users }) => (
+const App = ({ users, topHintUsers }) => (
     <div>
         <h1>Stats</h1>
         <ul>
@@ -26,27 +26,54 @@ const App = ({ users }) => (
             </li>
         ))}
         </ul>
+        <h2>Hints heroes</h2>
+        <ol>
+        {topHintUsers.map((user, i) => (
+            <li key={i}>
+                <strong>{user.userName}</strong> {user.hintsUsed}
+            </li>
+        ))}
+        </ol>
+
     </div>
 );
+
+const allTasks = ['task-1', 'task-2', 'task-3'];
 
 function render (snapshot) {
     const users = Object
         .keys(snapshot || {})
         .map(key => snapshot[key])
-        .map(obj => {
-            const tasks = ['task-1', 'task-2', 'task-3'].filter(key => {
-                return !(obj[key] && obj[key].start && obj[key].stop);
+        .map(userState => {
+            const tasks = allTasks.filter(key => {
+                return !(userState[key] && userState[key].start && userState[key].stop);
             });
 
-            if (obj[tasks[0]].start) {
-                obj.currentTask = tasks[0];
-                obj.subTask = obj[tasks[0]].subTaskId + 1;
+            if (userState[tasks[0]].start) {
+                userState.currentTask = tasks[0];
+                userState.subTask = userState[tasks[0]].subTaskId + 1;
             }
 
 
-            return obj;
+            return userState;
         });
 
+    const topHintUsers = users.map((user) => {
+        let hintsUsed = 0;
+        allTasks.forEach(taskId => {
+            if (!user[taskId] || !user[taskId].subTasks) { return 0; }
 
-    ReactDOM.render((<App users={users} />), document.getElementById('content'));
+            Object.keys(user[taskId].subTasks).forEach(subKey => {
+                const hints =  user[taskId].subTasks[subKey].hints;
+                if (hints) {
+                    hintsUsed += Object.keys(hints).length;
+                }
+            });
+        });
+        user.hintsUsed = hintsUsed;
+        return user;
+    }).sort((userA, userB) => (userA.hintsUsed > userB.hintsUsed ? 1 : -1))
+    .slice(0, 10);
+
+    ReactDOM.render((<App users={users} topHintUsers={topHintUsers} />), document.getElementById('content'));
 }
