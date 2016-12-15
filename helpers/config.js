0090b0a40585ae841c6d86b35dd20175914c179a
sveisvei <sveinung.rosaker@gmail.com>
2016-12-15 09:44:09 +0100

    set local ip

--- helpers/config.js
+++ helpers/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const AMQP_PORT = process.env.AMQP_PORT || 5672;
-const AMQP_URI = process.env.AMQP_URI || `amqp://192.168.56.94:${AMQP_PORT}`;
+const AMQP_PORT = process.env.AMQP_PORT || 8081;
+const AMQP_URI = process.env.AMQP_URI || `amqp://10.200.229.88:${AMQP_PORT}`;
 
 module.exports = {
     amqpUri: AMQP_URI,
